e349b7ccba726d72936b2451d92e27c7a7b2d71b
ShangYJQ <421207553@qq.com>
2026-05-05 16:47:29 +0800

    feat(fish): add fish config and prompt with git/hg support

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,68 @@
+if status is-interactive
+    # 基础环境变量
+    set -gx EDITOR nvim
+
+    set -gx FZF_DEFAULT_COMMAND 'fd --type f'
+
+    # LLVM / CMake
+    set -gx LDFLAGS -L/opt/homebrew/opt/llvm/lib
+    set -gx CPPFLAGS -I/opt/homebrew/opt/llvm/include
+    set -gx CMAKE_PREFIX_PATH /opt/homebrew/opt/llvm
+
+    # PATH
+    fish_add_path /opt/homebrew/opt/llvm/bin
+    fish_add_path /opt/homebrew/opt/curl/bin
+    fish_add_path (brew --prefix bison)/bin
+    fish_add_path (brew --prefix flex)/bin
+    fish_add_path /Users/yjq/.bun/bin
+    fish_add_path /Users/yjq/go/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/Library/Python/3.9/bin
+    fish_add_path $HOME/Library/Android/sdk/cmdline-tools/bin
+
+    # eza 替代 ls
+    alias ls='eza --icons --group-directories-first'
+    alias ll='eza --icons -l --git --header --total-size --time-style=long-iso'
+    alias la='eza --icons -l -a --git --header --total-size --time-style=long-iso'
+
+    function lt
+        set level 3
+
+        if test (count $argv) -gt 0
+            set level $argv[1]
+        end
+
+        eza --icons --tree --level=$level
+    end
+
+    alias c='clear'
+
+    abbr -a qkgit 'git add .; and aicommits -y; and git push'
+
+    if type -q zoxide
+        zoxide init fish | source
+    end
+
+    if type -q fzf
+        fzf --fish | source
+    end
+
+    # Mole shell completion
+    if type -q mole
+        mole completion fish 2>/dev/null | source
+    end
+
+    # yazi: 退出后 cd 到 yazi 当前目录
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    # ghcup
+    fish_add_path $HOME/.ghcup/bin
+    fish_add_path $HOME/.cabal/bin
+end
